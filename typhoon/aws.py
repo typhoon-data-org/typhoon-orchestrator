0fcd5e23ebef452c7cfa153c33365c47d4e1ae08
biellls <bielllobera@gmail.com>
2019-02-08 20:57:32 +0100

    Added connections creation, get and set

--- typhoon/aws.py
+++ typhoon/aws.py
@@ -1,6 +1,80 @@
+from typing import Union
+
 import boto3
+
+from typhoon import config
 
 
 def write_logs(body, bucket, key):
-    s3 = boto3.client("s3")
+    s3 = boto3.client('s3')
     s3.put_object(Body=body, Bucket=bucket, Key=key, ContentType='text/plain')
+
+
+def create_dynamodb_connections_table(env: str):
+    ddb = connect_dynamodb_metadata(env, 'resource')
+    table = ddb.create_table(
+        TableName='Connections',
+        KeySchema=[
+            {
+                'AttributeName': 'conn_id',
+                'KeyType': 'HASH'  # Partition key
+            },
+            # {
+            #     'AttributeName': 'title',
+            #     'KeyType': 'RANGE'  # Sort key
+            # }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'conn_id',
+                'AttributeType': 'S'
+            },
+            # {
+            #     'AttributeName': 'conn_type',
+            #     'AttributeType': 'S'
+            # },
+            # {
+            #     'AttributeName': 'host',
+            #     'AttributeType': 'S'
+            # },
+            # {
+            #     'AttributeName': 'port',
+            #     'AttributeType': 'N'
+            # },
+            # {
+            #     'AttributeName': 'login',
+            #     'AttributeType': 'S'
+            # },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        }
+    )
+    return table
+
+
+def connect_dynamodb_metadata(env: str, conn_type: str = 'resource'):
+    endpoint_url = config.get(env, 'dynamodb-endpoint', '')
+    extra_params = {}
+    if endpoint_url:
+        extra_params = {
+            'aws_access_key_id': 'dummy',
+            'aws_secret_access_key': 'dummy',
+            'endpoint_url': endpoint_url,
+            'region_name': 'us-west-2',
+        }
+
+    if conn_type == 'client':
+        ddb = boto3.client('dynamodb', **extra_params)
+    elif conn_type == 'resource':
+        ddb = boto3.resource('dynamodb', **extra_params)
+    else:
+        raise ValueError(f'Expected conn_type as client or resource, found: {conn_type}')
+    return ddb
+
+
+if __name__ == '__main__':
+    t = create_dynamodb_connections_table('dev')
+    a = 2
--- typhoon/connections.py
+++ typhoon/connections.py
@@ -2,11 +2,24 @@ import os
 from typing import NamedTuple, Optional
 
 import yaml
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 
+from typhoon.aws import connect_dynamodb_metadata, dict_to_ddb_item
 from typhoon.settings import get_env, typhoon_directory
 
 
 class ConnectionParams(NamedTuple):
+    conn_type: str
+    host: Optional[str] = None
+    port: Optional[int] = None
+    login: Optional[str] = None
+    password: Optional[str] = None
+    schema: Optional[str] = None
+    extra: Optional[dict] = None
+
+
+class Connection(NamedTuple):
+    conn_id: str
     conn_type: str
     host: Optional[str] = None
     port: Optional[int] = None
@@ -29,3 +42,27 @@ def get_connection_local(conn_id: str) -> ConnectionParams:
     with open(connections_yml, 'r') as f:
         connections = yaml.load(f)
     return ConnectionParams(**connections[conn_id])
+
+
+def set_connection(env: str, conn_id: str, conn_params: ConnectionParams):
+    ddb = connect_dynamodb_metadata(env, 'client')
+    serializer = TypeSerializer()
+    ddb.put_item(
+        TableName='Connections',
+        Item={
+            'conn_id': {'S': conn_id},
+            **serializer.serialize(conn_params._asdict())['M']
+        })
+
+
+def get_connection(env: str, conn_id: str) -> Connection:
+    ddb = connect_dynamodb_metadata(env, 'client')
+    response = ddb.get_item(
+        TableName='Connections',
+        Key={'conn_id': {'S': conn_id}}
+    )
+    if 'Item' not in response:
+        raise ValueError(f'Connection {conn_id} is not defined')
+    deserializer = TypeDeserializer()
+    conn = {k: deserializer.deserialize(v) for k, v in response['Item'].items()}
+    return Connection(**conn)
