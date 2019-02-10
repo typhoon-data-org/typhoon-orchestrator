2c1764f33233d4792337b07c07350ac14de5bbfb
biellls <bielllobera@gmail.com>
2019-02-10 17:45:33 +0100

    Added filter function

--- typhoon/contrib/functions/flow_control.py
+++ typhoon/contrib/functions/flow_control.py
@@ -1,6 +1,12 @@
-from typing import Sequence, Iterator
+from typing import Sequence, Iterator, Callable, Any
 
 
 def branch(branches: Sequence) -> Iterator:
     """Yields each item in the sequence"""
     return iter(branches)
+
+
+# noinspection PyShadowingBuiltins
+def filter(data: Any, filter_func: Callable[[Any], bool]) -> Iterator:
+    if filter_func(data):
+        yield data
