60dedd8dd3e7ddefe9beff20fb51fcfee05b99b0
biellls <bielllobera@gmail.com>
2019-02-10 17:53:35 +0100

    Fixed write_data signature

--- typhoon/contrib/functions/filesystem.py
+++ typhoon/contrib/functions/filesystem.py
@@ -4,6 +4,6 @@ from typhoon.contrib.hooks.filesystem_hooks import FileSystemHookInterface
 from typhoon.contrib.hooks.hook_factory import get_hook
 
 
-def write_data(conn_id: str, data: BytesIO, path: str):
+def write_data(data: BytesIO, conn_id: str, path: str):
     hook: FileSystemHookInterface = get_hook(conn_id)
     hook.write_data(data, path)
