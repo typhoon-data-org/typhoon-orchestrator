bfd807585abe1057aa2487a28b46fa36ef5c5447
biellls <bielllobera@gmail.com>
2019-03-16 20:38:29 +0100

    Added connections table

--- webserver/frontend/src/components/Connections.vue
+++ webserver/frontend/src/components/Connections.vue
@@ -1,40 +1,63 @@
 <template>
-  <!--<v-card>-->
-    <!--<v-card-title>-->
-      <!--Connections-->
-      <!--<v-spacer></v-spacer>-->
-      <!--<v-text-field-->
-        <!--v-model="search"-->
-        <!--append-icon="search"-->
-        <!--label="Search"-->
-        <!--single-line-->
-        <!--hide-details-->
-      <!--&gt;</v-text-field>-->
-    <!--</v-card-title>-->
-    <!--<v-data-table-->
-      <!--:headers="headers"-->
-      <!--:items="desserts"-->
-      <!--:search="search"-->
-    <!--&gt;-->
-      <!--<template v-slot:items="props">-->
-        <!--<td>{{ props.item.name }}</td>-->
-        <!--<td class="text-xs-right">{{ props.item.calories }}</td>-->
-        <!--<td class="text-xs-right">{{ props.item.fat }}</td>-->
-        <!--<td class="text-xs-right">{{ props.item.carbs }}</td>-->
-        <!--<td class="text-xs-right">{{ props.item.protein }}</td>-->
-        <!--<td class="text-xs-right">{{ props.item.iron }}</td>-->
-      <!--</template>-->
-      <!--<v-alert v-slot:no-results :value="true" color="error" icon="warning">-->
-        <!--Your search for "{{ search }}" found no results.-->
-      <!--</v-alert>-->
-    <!--</v-data-table>-->
-  <!--</v-card>-->
-  <h1>Connections</h1>
+  <v-container grid-list-md>
+    <v-card>
+      <v-card-title>
+        <v-icon large left>power</v-icon>
+        <h2>Connections</h2>
+        <v-spacer></v-spacer>
+        <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+          :headers="headers"
+          :items="connections"
+          :search="search"
+      >
+        <template v-slot:items="props">
+          <td>{{ props.item.conn_id }}</td>
+          <td>{{ props.item.conn_type }}</td>
+          <td>{{ props.item.host }}</td>
+          <td>{{ props.item.port }}</td>
+          <td>{{ props.item.login }}</td>
+          <td>{{ props.item.schema }}</td>
+        </template>
+        <v-alert v-slot:no-results :value="true" color="error" icon="warning">
+          Your search for "{{ search }}" found no results.
+        </v-alert>
+      </v-data-table>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
   export default {
-    name: "Connections"
+    name: "Connections",
+    data: () => ({
+      search: '',
+      headers: [
+        { text: 'Connection ID', align: 'left', value: 'conn_id' },
+        { text: 'Type', value: 'conn_type' },
+        { text: 'Host', value: 'host' },
+        { text: 'Port', value: 'port' },
+        { text: 'Login', value: 'login' },
+        { text: 'Schema', value: 'schema' },
+      ],
+      connections: [
+        {
+          conn_id: 'reminder_bot',
+          conn_type: 'telegram',
+          host: 'telegram.com',
+          port: '1234',
+          login: 'jkdfnkjabs36814qwhasndjn',
+          schema: 'None'
+        }
+      ],
+    }),
   }
 </script>
 
