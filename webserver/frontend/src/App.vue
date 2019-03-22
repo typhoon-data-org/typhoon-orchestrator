760c73d2185026b82931b5a22f2529957297b2ea
biellls <bielllobera@gmail.com>
2019-03-22 18:48:58 +0100

    Added link to project

--- webserver/frontend/src/App.vue
+++ webserver/frontend/src/App.vue
@@ -13,10 +13,10 @@
       <v-spacer></v-spacer>
       <v-btn
           flat
-          href="https://github.com/vuetifyjs/vuetify/releases/latest"
+          href="https://github.com/biellls/typhoon-orchestrator"
           target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Pre-Alpha</span>
       </v-btn>
     </v-toolbar>
 
