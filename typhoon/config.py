00d19778259197436f7053953c7f3f172d520158
biellls <bielllobera@gmail.com>
2019-04-22 18:45:34 +0200

    New config with typhoon and deployment files separated

--- typhoon/config.py
+++ typhoon/config.py
@@ -4,18 +4,50 @@ from configparser import ConfigParser
 from typhoon.settings import typhoon_directory
 
 
-def settings_path():
-    return os.path.join(typhoon_directory(), 'typhoonconfig.cfg')
+class Config:
+    def __init__(self, path, env):
+        self.path = path
+        self.env = env
+        self.config_dict = ConfigParser().read(self.path)
+
+    def reload(self):
+        self.config_dict = ConfigParser().read(self.path)
+
+    def get(self, var, default=None, mandatory=False):
+        if mandatory and var not in self.config_dict[self.env].keys():
+            raise ValueError(f'No attribute {var} in {self.env} config for {self.path}')
+        return self.config_dict[self.env].get(var, default)
 
 
-def read_config():
-    config = ConfigParser()
-    config.read(settings_path())
-    return config
+class TyphoonConfig:
+    """Parameters related to Typhoon Orchestrator configuration used at run-time (eg: DAG executions)"""
+    def __init__(self, env):
+        self.env = env
+        self.config = Config(os.path.join(typhoon_directory(), 'typhoonconfig.cfg'), env)
+
+    @property
+    def logger(self):
+        """Mandatory parameter defining what kind of Logger we will be using"""
+        return self.config.get('logger', mandatory=True)
+
+    @property
+    def local_db(self):
+        """If you are running a local instance of DynamoDB specify the endpoint"""
+        return self.config.get('local-db')
 
 
-def get(env, var, default=None, mandatory=False):
-    config = read_config()
-    if mandatory and var not in config['env'].keys():
-        raise ValueError(f'No attribute {var} in {env} config')
-    return config[env].get(var, default)
+class DeployConfig:
+    """Parameters related to deploying an instance of Typhoon Orchestrator"""
+    def __init__(self, deploy_env):
+        self.env = deploy_env
+        self.config = Config(os.path.join(typhoon_directory(), 'deployconfig.cfg'), deploy_env)
+
+    @property
+    def aws_profile(self):
+        """Profile used to deploy Typhoon to the specified environment"""
+        return self.config.get('aws-profile', mandatory=True)
+
+    @property
+    def project_name(self):
+        """Name of the project used to give a unique name to resources in AWS"""
+        return self.config.get('project-name', default='project')
