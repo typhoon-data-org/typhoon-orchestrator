2aa84aadec826843696741bd87bb6b64f9ad81e6
biellls <bielllobera@gmail.com>
2019-08-16 22:09:47 +0200

    Made metadata store interface abstract

--- typhoon/core/metadata_store_interface.py
+++ typhoon/core/metadata_store_interface.py
@@ -1,16 +1,23 @@
+from abc import ABC, abstractmethod
+
 from typhoon.connections import Connection
 from typhoon.variables import Variable
 
 
-class MetadataStoreInterface:
+class MetadataStoreInterface(ABC):
+    """All implementations of the metadata store must override these functions"""
+    @abstractmethod
     def get_connection(self, conn_id: str) -> Connection:
         pass
 
+    @abstractmethod
     def set_connection(self, conn: Connection):
         pass
 
+    @abstractmethod
     def get_variable(self, variable_id: str) -> Variable:
         pass
 
+    @abstractmethod
     def set_variable(self, variable: Variable):
         pass
