da2f32167f3a5da9882b3ca8a4d605e7c807ae2f
biellls <bielllobera@gmail.com>
2021-04-26 21:46:16 +0200

    Moved database contrib to extension

--- typhoon/contrib/functions/bulk.py
+++
@@ -1,5 +0,0 @@
-from typhoon.contrib.hooks.dbapi_hooks import BigQueryHook
-
-
-def load_csv(hook: BigQueryHook, table: str, path: str):
-    hook.load_csv(table, path)
--- typhoon/contrib/templates/sql.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from typhoon.core.templated import Templated
-
-
-@dataclass
-class SelectAll(Templated):
-    template = 'SELECT * FROM {{ table_name }}'
-    table_name: str
