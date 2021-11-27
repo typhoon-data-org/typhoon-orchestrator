9c9f1539c14386ac1e3d69802f30aadcdadaf03a
grimfandangle <82180156+grimfandangle@users.noreply.github.com>
2021-11-27 16:44:13 +0100

    remove fuzzy wuzzy for now

--- typhoon/examples/hello_world/functions/open_library_api.py
+++ typhoon/examples/hello_world/functions/open_library_api.py
@@ -40,14 +40,14 @@ def get_works(
     response = requests.get(full_endpoint, params=params)
     yield response.json()
 
-from fuzzywuzzy import fuzz
-
-def is_similar_name(requested_author: str, returned_author: str) -> bool:
-    r = fuzz.ratio(requested_author.lower(),returned_author.lower())
-    print('')
-    print (f'Scoring of r  {r}')
-    print('')
-    return True if r > 50 else False 
+# from fuzzywuzzy import fuzz
+#
+# def is_similar_name(requested_author: str, returned_author: str) -> bool:
+#     r = fuzz.ratio(requested_author.lower(),returned_author.lower())
+#     print('')
+#     print (f'Scoring of r  {r}')
+#     print('')
+#     return True if r > 50 else False
 
 
 
