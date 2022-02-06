116810439b2bd3cabb8e2ad01bf8073582e1ca17
biellls <bielllobera@gmail.com>
2022-02-06 18:39:36 +0100

    Fix circular import

--- typhoon/contrib/hooks/hook_interface.py
+++ typhoon/contrib/hooks/hook_interface.py
@@ -1,13 +1,15 @@
 from typing_extensions import Protocol, runtime_checkable
 
-from typhoon.connections import ConnectionParams
+import typing
+if typing.TYPE_CHECKING:
+    from typhoon.connections import ConnectionParams
 
 
 @runtime_checkable
 class HookInterface(Protocol):
     conn_type: str
-    conn_params: ConnectionParams
+    conn_params: 'ConnectionParams'
 
-    def __init__(self, conn_params: ConnectionParams): ...
+    def __init__(self, conn_params: 'ConnectionParams'): ...
     def __enter__(self): ...
     def __exit__(self, exc_type, exc_val, exc_tb): ...
